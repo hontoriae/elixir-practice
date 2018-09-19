740a9629ff7e0a8e4df88f9723292953493ad3c8
Emily Hontoria <hontoria@ccs.neu.edu>
2018-09-19 18:30:02 -0400

    Playing around with factor

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,7 +1,16 @@
 defmodule Practice.Factor do
   def factor(expr) do
-    expr
+    isPrime(expr)
   end
 
-
+  def isPrime(x) do
+    i = 1
+    acc = (x >= 1)
+      unless (i > :math.sqrt(x)) do
+        # i = i + 1
+        # acc = acc && !(rem(x, i) == 0)
+        acc = false
+      end
+    acc
+  end
 end
